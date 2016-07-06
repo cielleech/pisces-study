8e43bf18bedb74859d5ef2bac7d8861aecdb8d47
cielleech <lancelot.q.lee@outlook.com>
2016-07-06 22:32:25 +0800

    1. change maven project to gradle project

---
+++ src/main/scala/AppMain.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by cielleech on 16/6/28.
+  */
+object AppMain {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
---
+++ src/main/scala/com/manning/gia/todo/web/ToDoServlet.scala
@@ -0,0 +1,12 @@
+package com.manning.gia.todo.web
+
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+
+/**
+  * Created by cielleech on 16/6/28.
+  */
+class ToDoServlet extends HttpServlet {
+  protected override def service(request: HttpServletRequest, response: HttpServletResponse): Unit = {
+    val servletPath = request.getServletPath;
+  }
+}
