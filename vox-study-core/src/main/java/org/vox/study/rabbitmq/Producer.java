f82bc7eb7952c91255f932e5a5e23ad2b0e75c79
cielleech <cielleech@qq.com>
2017-11-01 04:15:02 +0800

    update for scala version

--- vox-study-core/src/main/java/org/vox/study/rabbitmq/Producer.java
+++ vox-study-core/src/main/java/org/vox/study/rabbitmq/Producer.java
@@ -1,16 +1,16 @@
 package org.vox.study.rabbitmq;
 
-//import com.rabbitmq.client.Connection;
-//import com.rabbitmq.client.ConnectionFactory;
-//
-//import java.io.IOException;
-//import java.util.concurrent.TimeoutException;
-//
-//public class Producer {
-//    public void send() throws IOException, TimeoutException {
-//        ConnectionFactory factory = new ConnectionFactory();
-//
-//        factory.setHost("127.0.0.1");
-//        Connection connection = factory.newConnection();
-//    }
-//}
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer {
+    public void send() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost("127.0.0.1");
+        Connection connection = factory.newConnection();
+    }
+}
